10ce48396dfc148702095a226cb535aba80cd642
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-09-20 01:26:41 +0500

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,9 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: "swagger/json"
+  });
   await app.listen(3000);
 }
 bootstrap();
